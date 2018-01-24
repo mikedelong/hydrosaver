fab96cc2e063032ce75057eee1792751606e1cfd
Mike DeLong <mikedelong@outlook.com>
2018-01-24 16:48:22 -0500

    feature: set up logging.

---
+++ reader.py
@@ -0,0 +1,13 @@
+import logging
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+input_folder = './input/'
